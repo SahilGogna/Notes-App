99bfcaabb1ec34eb0399453d0e54095fb94c27ed
Sahil Gogna <gogna.sahil95@gmail.com>
2019-04-08 23:27:57 -0400

    firebase login signup added

--- messagingapp/messagingapp/LoginViewController.swift
+++ messagingapp/messagingapp/LoginViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import FirebaseAuth
+
 
 class LoginViewController: UIViewController {
 
@@ -20,6 +22,8 @@ class LoginViewController: UIViewController {
     
     @IBOutlet weak var signInButton: UIButton!
     
+    var isSignIn: Bool = true
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,9 +33,51 @@ class LoginViewController: UIViewController {
     
     @IBAction func signInSelectorChanged(_ sender: UISegmentedControl) {
         
+        //flip the boolean
+        isSignIn = !isSignIn
+        
+        //change the label
+        if isSignIn{
+            signInLabel.text = "SignIn"
+            signInButton.setTitle("SignIn", for: .normal)
+        }else{
+            signInLabel.text = "Register"
+            signInButton.setTitle("Register", for: .normal)
+        }
+        
     }
 
     @IBAction func signInButtonTapped(_ sender: UIButton) {
         
+        //TODO: Do some form validation on the email and the password
+        
+        if let email = emailTextField.text, let pass = passwordTextField.text{
+            //check if it is register or SignIn
+            if isSignIn{
+                //signIn the user with the firebase
+                Auth.auth().signIn(withEmail: email, link: pass) { (user, error) in
+                    // check if the user is not null
+                    if let u = user{
+                        //user is found , go to home screen
+                        self.performSegue(withIdentifier: "goToAllMessages", sender: self)
+                    }else{
+                        //Error: check error and show message
+                    }
+                }
+            }else{
+                // register the user with the firebase
+                Auth.auth().createUser(withEmail: email, password: pass) { (user, error) in
+                    // check if the user is not null
+                    if let u = user{
+                        //user is found , go to home screen
+                        self.performSegue(withIdentifier: "goToDashboard", sender: self)
+                    }else{
+                        //Error: check error and show message
+                        print("\(error)")
+                    }
+                }
+                
+            }
+        }
     }
 }
